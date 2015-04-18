e9e11105b47d988d8dadacbe07eb40e38c35bb80
Joseph Brady <escalade2005@hotmail.com>
2015-04-18 12:56:09 -0500

    Initial commit

---
+++ hrFind-Hackerrank/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace hrFind_Hackerrank
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numCases = Int32.Parse(Console.ReadLine());
+            var start = new Regex(@"^hackerrank", RegexOptions.Compiled);
+            var end = new Regex(@"hackerrank$", RegexOptions.Compiled);
+            var both = new Regex(@"^hackerrank.*hackerrank$", RegexOptions.Compiled);
+            while (numCases-- > 0)
+            {
+                var str = Console.ReadLine();
+                var answer = -1;
+                if (both.IsMatch(str))
+                {
+                    answer = 0;
+                }
+                else if (start.IsMatch(str))
+                {
+                    answer = 1;
+                }
+                else if (end.IsMatch(str))
+                {
+                    answer = 2;
+                }
+                Console.WriteLine(answer);
+            }
+        }
+    }
+}
